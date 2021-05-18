3e651468f4d1de6915d6c6d9a2d6b7b37e725f0b
firatkilinc7 <55871405+firatkilinc7@users.noreply.github.com>
2021-05-18 13:30:25 +0300

    Create login.php
    feat: added login page

---
+++ login.php
@@ -0,0 +1,68 @@
+<?php
+   include("config.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($link,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($link,$_POST['password']); 
+      
+      $sql = "SELECT Tc FROM users WHERE Kullanici_Adi = '$myusername' and Password = '$mypassword'";
+      $result = mysqli_query($link,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+        // session_register($myusername);
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Login</h2>
+        
+        <?php 
+        if(!empty($login_err)){
+            echo '<div class="alert alert-danger">' . $login_err . '</div>';
+        }        
+        ?>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Kullanici AdÄ±</label>
+                <input type="text" name="username">
+           </div>    
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password">
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+    </div>
+</body>
+</html>
